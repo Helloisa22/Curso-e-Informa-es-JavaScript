c674628251dff8bf96dc3de28525c25900e15af9
Heloisa Costa <helloisa.betriz44@gmail.com>
2019-12-26 18:54:54 -0300

    Atualizando arquivos e adicionando a pasta IntroducaoWeb

--- Exercicios.JS/objeto/jsonVsObj.js
+++
@@ -1,7 +0,0 @@
-const obj = { a: 1, b: 2, c: 3, soma() {return a + b + c }}
-console.log(JSON.stringify(obj)) // O JSON É UM FORMATO DE DADOS ELE NÃO É ALGO QUE É EXECUTAVEL // TRAFEGAR
-
-// console.log(JSON.parse(' { a: 1, b: 2, c: 3} '))
-// console.log(JSON.parse(" {'a': 1, 'b': 2, 'c':3} "))
-// console.log(JSON.parse(' {"a": 1, "b" 2, "c": 3} '))
-console.log(JSON.parse(' {"a": 1, "b": 2, "c": 3} '))
